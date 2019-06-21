b8ef385f7444cbedaef92151fd548087b92b82be
Vipin Kumar <vipikum3@cisco.com>
2019-06-21 11:15:55 +0530

    initial setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { VerticalTabComponent } from './vertical-tab/vertical-tab.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VerticalTabComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/vertical-tab/vertical-tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vertical-tab',
+  templateUrl: './vertical-tab.component.html',
+  styleUrls: ['./vertical-tab.component.scss']
+})
+export class VerticalTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
